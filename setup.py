04ad54edf78fcfdc05ecd6e5bf50d5f7f767dedf
natiiix <natiwastaken@gmail.com>
2019-07-30 19:50:42 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='postgresql',
+    version='0.1',
+    description='Lightweight psycopg2 wrapper focused on ease of use',
+    url='http://github.com/natiiix/postgresql',
+    author='Ivo Meixner',
+    author_email='natiwastaken@gmail.com',
+    license='MIT',
+    packages=['postgresql'],
+    zip_safe=False
+)

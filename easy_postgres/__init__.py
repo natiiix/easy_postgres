2ec238e6a78912c1259e261937d038757101e262
Ivo Meixner <natiwastaken@gmail.com>
2019-08-13 14:34:05 +0200

    Add docstring to main module

--- easy_postgres/__init__.py
+++ easy_postgres/__init__.py
@@ -1,2 +1,10 @@
+"""
+Easy-Postgres Library
+---------------------
+
+Abstraction layer for PostgreSQL database
+manipulation based on the `psycopg2` package.
+"""
+
 from .connection import Connection
 from .dictionary import Dictionary

5a9bc538f31fe8c2f47650d7c3c587222ab6ca81
Ivo Meixner <natiwastaken@gmail.com>
2019-08-13 14:45:38 +0200

    Add docstrings to Transaction

--- easy_postgres/transaction.py
+++ easy_postgres/transaction.py
@@ -1,15 +1,35 @@
+"""Module containing the Transaction class."""
+
+
 class Transaction:
+    """Pseudo-transaction, which pauses the PostgreSQL auto-commit setting."""
+
     def __init__(self, conn):
+        """
+        Initialize a new transaction.
+
+        Avoid creating multiple transactions for the same connection
+        at the same time. This could lead to race conditions and
+        some changes could be accidentally committed or
+        rolled back by one of the other transactions.
+        """
         self.conn = conn
         self.autocommit = conn.conn.autocommit
 
     def __enter__(self):
+        """Turn off auto-commit and return the transaction instance."""
         if self.autocommit:
             self.conn.conn.autocommit = False
 
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
+        """
+        Commit or roll-back and reset the auto-commit setting.
+
+        Changes will be committed if no exception has been thrown.
+        Otherwise a roll-back of this transaction will happen.
+        """
         # Roll-back on an exception
         if exc_type or exc_value or exc_traceback:
             self.conn.rollback()
